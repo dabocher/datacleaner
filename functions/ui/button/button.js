43bfd6d39532402aa764a55ffecbbb82b3d53d9e
David Botello <dabocher@gmail.com>
2025-04-06 10:04:58 +0200

    Change Format&Descarrega button background color

--- functions/ui/button/button.js
+++ functions/ui/button/button.js
@@ -3,7 +3,7 @@ import { formatAndSaveData } from "../../fileFormatter/fileSaver/formattedFileSa
 export const addFormatButton = (data) => {
   const fileInfo = window.fileInfo;
   const formatBtn = document.createElement("button");
-  formatBtn.className = "upload-btn";
+  formatBtn.className = "upload-btn active";
   formatBtn.innerHTML =
     '<i class="fas fa-file-export"></i> Format & Descàrrega';
   formatBtn.onclick = () => formatAndSaveData(data);
